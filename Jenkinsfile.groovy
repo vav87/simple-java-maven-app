97d3161b986a7ee76099e1dec534b93796f56967
Aleksandr Vdovin <alksr87@gmail.com>
2019-12-16 14:07:23 +0300

    fix 8

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,10 +1,12 @@
 node {
+    git url: 'https://github.com/vav87/simple-java-maven-app.git', branch: env.BRANCH_NAME
+
     stage('Build') {
         if (env.BRANCH_NAME == 'master') {
-                echo 'I only execute on the master branch'
-                bat 'mvn -B -DskipTests clean package'
-            } else {
-                echo 'I execute elsewhere'
+            echo 'I only execute on the master branch'
+            bat 'mvn -B -DskipTests clean package'
+        } else {
+            echo 'I execute elsewhere'
         }
     }
     stage('Test') {
